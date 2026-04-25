5c1cea25153863d8b31e8fd70117cdc75d8d3f82
malkiaman-dev <malkiaman.profile@gmail.com>
2026-04-26 00:11:20 +0500

    Fixed backend imports and structure

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,8 @@
+import sys
 import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
 import cv2
 import numpy as np
 from fastapi import FastAPI, File, HTTPException, UploadFile
